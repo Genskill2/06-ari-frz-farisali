524c8c60c93da96544c758e1a944d3d9d0351cd6
FARIS ALI <66835333+frz-farisali@users.noreply.github.com>
2021-07-02 10:26:48 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,26 +1,31 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<cs50.h>
+#include<string.h>
+
 string ari(string);
 string ari(string s)
 {
-int character=0;
+int characters=0;
 int words=0;
-int sentence=0;
+int sentences=0;
 int ari;
-char* gradelevel[14]={"Kindergarten","First/Second Grade","Third Grade","Fourth Grade","Fifth Grade","Sixth Grade","Seventh Grade","Eighth Grade","Ninth Grade","Tenth Grade","Eleventh Grade","Twelfth grade","College student","Professor");
-for(int=0; i<strlen(s); i++)
+char* gradelevel[14] = {"Kindergarten", "First/Second Grade", "Third Grade", "Fourth Grade", "Fifth Grade", "Sixth Grade", "Seventh Grade", "Eighth Grade", "Ninth Grade", "Tenth Grade", "Eleventh Grade", "Twelfth grade", "College Student", "Professor"};
+for(int i=0; i < strlen(s); i++) 
 {
-if(isalnum(s[i]))
-{
-character++;
-}
-else if(s[i]==' ')
-{
-words++;
-}
-else if(s[i]=='.'||s[i]=='?'||s[i]=='!')
-{
-sentence++;
-}
-ari=4.71*((float)character/words)+0.5*((float)words/sentence)-21.43;
+    if(isalnum(s[i]))
+    {
+        characters++;
+    }
+    else if(s[i]==' ')
+    {
+        words++;
+    }
+    else if(s[i]=='.' || s[i]=='!' || s[i]=='?') 
+    {
+        sentences++;
+    }
+ari = 4.71*((float)characters/words) + 0.5*((float)words/sentences) - 21.43;
 }
 return gradelevel[ari];
 }
